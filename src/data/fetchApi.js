2b52a3b6898913ea6279c87e832d6bc7f2b4d06d
mohamed idmoulay <mohamedidmoulay99@gmail.com>
2023-01-12 13:06:23 +0000

    created album components

--- src/data/fetchApi.js
+++ src/data/fetchApi.js
@@ -3,9 +3,7 @@ import axios from 'axios';
 
 function fetchApi(segment, callback) {
   axios.get(`https://jsonplaceholder.typicode.com/${segment}`)
-    .then(res => {
-      callback(res.data)
-    });
+    .then(res => callback(res.data));
 }
 
 export default fetchApi
